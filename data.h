9788013da8535cce537090b5190191562b916f8c
Grégoire Ménard <108455082+greg-bioinfo@users.noreply.github.com>
2022-11-01 20:19:58 +0100

    Add files via upload

--- data.h
+++ data.h
@@ -1,6 +1,8 @@
 #ifndef A_UNIQUE_HEADER_FILE_GUARD_NAME
 #define A_UNIQUE_HEADER_FILE_GUARD_NAME
 #include <string>
+#include <fstream>
+
 
 struct bornes{
     int min;
@@ -18,13 +20,18 @@ bornes def_bornes(int index, std::string exp);
 
 ancre def_ancre (int index, std::string exp);
 
-void expression_0(int min, int max);
+void expression_0(int min, int max, std::string& trace, int length_max);
 
-void expression_1(int min, int max);
+void expression_1(int min, int max, std::string& trace, int length_max);
 
-void expression_2(int min, int max);
+void expression_2(int min, int max, std::string& trace, int length_trace);
 
-void expression_3(int min,int max,ancre anc, std::string exp);
+void expression_3(int min,int max,ancre anc, std::string exp, std::string& trace, int length_max);
 
+void expression_4(int min,int max,ancre anc, std::string exp, std::string& trace, int length_max);
+
+void expression_5(int min,int max,ancre anc, std::string exp, std::string& trace, int length_max);
+
+int length_trace(std::string trace);
 
 #endif
