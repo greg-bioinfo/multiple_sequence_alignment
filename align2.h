c631049048a2df0fd3e22d95b31165300bdd3256
Grégoire Ménard <108455082+greg-bioinfo@users.noreply.github.com>
2023-01-26 11:47:48 +0100

    Add files via upload

--- align2.h
+++ align2.h
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <vector>
 
-std::vector<std::vector<int>> Matrix_cost(std::vector<std::string>& s1, std::vector<std::string>& s2,int del_penal, int ins_penal);
+std::vector<std::vector<int>> Matrix_cost(std::vector<std::string>& s1, std::vector<std::string>& s2,int del_penal, int ins_penal,int match_score,int mismatch_score);
 
 std::vector<std::string> Extract_event(std::string);
 
@@ -15,11 +15,11 @@ void reverse_vec(std::vector<std::string>& vec);
 
 void Display_alignment(int** M,std::vector<std::string> s1,std::vector<std::string> s2,int del_penal,int ins_penal);
 
-int Score_alignment(std::vector<std::string>& s1, std::vector<std::string>& s2, int del_penal, int ins_penal);
+int Score_alignment(std::vector<std::string>& s1, std::vector<std::string>& s2, int del_penal, int ins_penal,int match_score,int mismatch_score);
 
-std::vector<std::vector<int>> Matrix_dissimm(std::vector<std::vector<std::vector<std::string>>>& vec, int del_penal, int ins_penal);
+std::vector<std::vector<int>> Matrix_dissimm(std::vector<std::vector<std::vector<std::string>>>& vec, int del_penal, int ins_penal,int match_score,int mismatch_score);
 
-std::vector<std::vector<int>> Matrix_dissimm2(std::vector<std::vector<std::string>>& vec, int del_penal, int ins_penal);
+std::vector<std::vector<int>> Matrix_dissimm2(std::vector<std::vector<std::string>>& vec, int del_penal, int ins_penal,int match_score,int mismatch_score);
 
 struct Coord{
     int x;
@@ -28,20 +28,30 @@ struct Coord{
 
 Coord Maximum_matrix(std::vector<std::vector<int>>& M, int m);
 
-std::vector<std::vector<int>> Matrix_cost2(std::vector<std::vector<std::string>>& s1, std::vector<std::vector<std::string>>& s2, int del_penal, int ins_penal);
+std::vector<std::vector<int>> Matrix_cost2(std::vector<std::vector<std::string>>& s1, std::vector<std::vector<std::string>>& s2, int del_penal, int ins_penal,int match_score,int mismatch_score);
 
-void Alignement_projection (std::vector<std::vector<std::string>>& classe1, std::vector<std::vector<std::string>>& classe2, std::vector<std::vector<int>> M, int del_pena, int ins_penal);
+std::vector<std::vector<std::string>> Alignment_projection (std::vector<std::vector<std::string>>& classe1, std::vector<std::vector<std::string>>& classe2, std::vector<std::vector<int>> M, int del_pena, int ins_penal);
 
-void affichage(std::vector<std::vector<std::string>>& al);
+void Display(std::vector<std::vector<std::string>>& al);
+
+void write_alignment(std::vector<std::vector<std::string>>& al,std::string path);
+
+int Score_alignment_class(std::vector<std::vector<std::string>>& class1, std::vector<std::vector<std::string>>& class2, int del_penal,int ins_penal,int match_score,int mismatch_score);
+
+void Update_M_dissim(std::vector<std::vector<int>>& M_dissim,std::vector<std::vector<std::string>>& new_classe,std::vector<std::vector<std::vector<std::string>>>& vec_seq, int index1, int index2, int del_penal, int ins_penal,int match_score,int mismatch_score);
 
 float  difference(std::vector<std::vector<int>> M1, std::vector<std::vector<int>> M2, int length);
 
-float score_alignement_multiple(std::vector<std::vector<std::string>>& al,int del_penal,int ins_penal);
+void initialization_matrix(std::vector<std::vector<int>>& M,int n, int m);
 
-std::vector<std::vector<std::string>> ACH(std::vector<std::vector<std::string>>& vec_seq,int del_penal,int ins_penal);
+float score_alignment_multiple(std::vector<std::vector<std::string>>& al,int del_penal,int ins_penal,int match_score,int mismatch_score);
 
-double Alignement_multiple(std::vector<std::vector<std::string>>& al,float seuil,std::string path,int del_penal, int ins_penal );
+void ACH(std::vector<std::vector<std::string>>& vec_seq,int del_penal,int ins_penal,int match_score,int mismatch_score);
 
-void carac_csv (std::string path, std::vector<std::vector<std::string>>al, int del_penal, int ins_penal, float time);
+void Alignment_multiple(std::vector<std::vector<std::string>>& al,float seuil,std::string path,int del_penal, int ins_penal,int match_score,int mismatch_score);
+
+int nb_event_per_column(std::vector<std::vector<std::string>>align);
+
+void carac_csv (std::string path, std::vector<std::vector<std::string>>al, int del_penal, int ins_penal,int match_score,int mismatch_score,float time,std::string path_align);
 
 #endif
